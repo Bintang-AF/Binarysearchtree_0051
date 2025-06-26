99e9969b4e54f651690c16ff320cfb6011665191
Bintang-AF <bintang.aulia.ft24@mail.umy.ac.id>
2025-06-26 11:07:03 +0700

    menginisial left dan right

--- binarytree.cpp
+++ binarytree.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 using namespace std;
 
+class node 
+{
+    public:
+    int info;
+    node *leftchild;
+    node *rightchild;
+
+    node ()
+    {
+        leftchild = nullptr;
+        rightchild = nullptr;
+    }
+};
